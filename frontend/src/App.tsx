3298c9b68b0e012eee4f83e25bb4e2920520d534
trynalearn-code <michaelshloush@gmail.com>
2026-09-17 17:22:42 +0300

    did some css styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,16 @@ import Login from "./Login"
 import { Route, BrowserRouter, Routes, Link } from 'react-router-dom'
 import Register from "./Register"
 import Home from "./Home"
-import "./Apps.css"
+
+import "./style.css"
 
 function App() {
   return (
     <>
+    <div>
       <Header />
+    </div>
+    <main>
       <BrowserRouter>
         <nav >
           <Link to="/">Home</Link>
@@ -18,12 +22,15 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/" element={< Home/>} />
+          <Route path="/" element={< Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
+    </main>
+    <footer>
       < Footer />
+    </footer>
     </>
   )
 }
--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <>
-    <div>Home</div>
+    <h1>Home</h1>
     <h3>Awesome Online School was created by 3 brilliant masterminds yada yada yada... can't wait for you to join!</h3>
     </>
   )

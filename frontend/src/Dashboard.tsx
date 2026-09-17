80cca832a77ab605368aae017b134c8657ec0f89
trynalearn-code <michaelshloush@gmail.com>
2026-09-17 17:37:18 +0300

    fixed bug

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -4,15 +4,13 @@ function Dashboard() {
     const clear = () => {
     localStorage.clear();
   }
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
-  const [email, setEmail] = useState()
+
   return (
     <>
     <h1>Dashboard</h1>
-    <h3>username: {username}</h3>
-    <h3>password: {password}</h3>
-    <h3>email: {email}</h3>
+    <h3>username: {}</h3>
+    <h3>password: {}</h3>
+    <h3>email: {}</h3>
     <button onClick={clear}>Log Out</button>
     </>
   )
